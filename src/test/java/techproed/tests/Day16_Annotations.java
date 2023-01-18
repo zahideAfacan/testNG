f58dee8e3f2ffeea1f98e55796956fa7a5c35a0e
zahideAfacan <zahideafacan66@gmail.com>
2023-01-17 19:03:24 -0500

    data provider

--- src/test/java/techproed/tests/Day16_Annotations.java
+++ src/test/java/techproed/tests/Day16_Annotations.java
@@ -1,25 +1,26 @@
 package techproed.tests;
 
 import org.testng.annotations.*;
-
 public class Day16_Annotations {
     /*
-    suite> tet> group> class> method
-    @Test : create test case
-    @Before and @After :10 before and after annotations. there are used to comtrol FLOW AND DESIGN of the test
-    @Ignore " skip the test case
-    @Test(enable=false) : Disable the test case. similar the @Ignore
-    @Test(priority=number): prioritize/order test case execution.
+    suite > test > group > class > method
+    @Test : creates test case
+    @Before and @After : 10 before and after annotations. The are used to control FLOW and DESIGN of the tests
+    @Ignore : skip test case
+    @Test(enable=false) : Disable the test case. By default enable=true.
+    @Test(priority=number) : prioritize/order test case execution.
     NOTE: Tests that has no priority parameter has a priority of 0. Test(priority=0)
-    TestNG test cases runs in alphabetical order by default
+    TestNG test cases runs in alphabetical order by default.
+    We should use priority when there are multiple test cases in the same class.
+    -3, 0(ALSO TESTS WITH NO PRIORITY), 4, 9
      */
     @BeforeSuite
     public void beforeSuite(){
-        System.out.println("before suite");
+        System.out.println("Before Suite");
     }
     @AfterSuite
     public void afterSuite(){
-        System.out.println("after suite");
+        System.out.println("After Suite");
     }
     @BeforeTest
     public void beforeTest(){
@@ -45,8 +46,7 @@ public class Day16_Annotations {
     public void afterMethod(){
         System.out.println("After Method");
     }
-
-    @Test
+    @Test(priority = 2)
     public void test1(){
         System.out.println("test1");
     }
@@ -55,16 +55,20 @@ public class Day16_Annotations {
     public void test2(){
         System.out.println("test2");
     }
-    @Test
+    @Test(enabled = false)
     public void test3(){
         System.out.println("test3");
     }
-    @Test(enabled=false)
+    @Test(priority = 3)
     public void test4(){
         System.out.println("test4");
     }
-    @Test(priority=1)
+    @Test(priority = 1,groups = "minor-regression-group")
     public void test5(){
         System.out.println("test5");
+    }
+    @Test(groups = "minor-regression-group")
+    public void test6(){
+        System.out.println("test6");
     }
 }
--- src/test/java/techproed/tests/smoketest/logintests/Day19_NegativeLoginTest.java
+++ src/test/java/techproed/tests/smoketest/logintests/Day19_NegativeLoginTest.java
@@ -8,71 +8,71 @@ import techproed.pages.HomePage;
 import techproed.pages.LoginPage;
 import techproed.utilities.ConfigReader;
 import techproed.utilities.Driver;
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import techproed.pages.HomePage;
+import techproed.pages.LoginPage;
+import techproed.utilities.ConfigReader;
+import techproed.utilities.Driver;
 import techproed.utilities.ReusableMethods;
-
 import java.io.IOException;
-
 public class Day19_NegativeLoginTest {
     HomePage homePage;
     LoginPage loginPage;
     Faker faker;
-
-    @Test
-    public void US100208_Negative_LoginTest() throws IOException {
-//Name:
-//US100208_Negative_Login
-//Description:
-        //  As customer, I should not be able to log in the application
+    @Test(groups = "minor-regression-group")
+    public void US100208_Negative_Login() throws IOException {
+//        As customer, I should not be able to log in the application
+        //going blue rental car home page
         Driver.getDriver().get(ConfigReader.getProperty("app_home_url"));
-        //        click on home page login link
+//        click on home page login link
         homePage = new HomePage();
-        loginPage=new LoginPage();
+        loginPage= new LoginPage();
         ReusableMethods.waitFor(3);
-        //  Customer email: fake@bluerentalcars.com
         homePage.homePageLoginLink.click();
+//        sending credentials and clicking on login button
         ReusableMethods.waitFor(3);
-        loginPage.userName.sendKeys("fake@bluerentalcars.com ");
+//        Customer email: fake@bluerentalcars.com
+//        Customer password: fakepass
+        loginPage.userName.sendKeys("fake@bluerentalcars.com");
         ReusableMethods.waitFor(3);
-        //  Customer password: fakepass
         loginPage.password.sendKeys("fakepass");
         ReusableMethods.waitFor(3);
         loginPage.loginButton.click();
         ReusableMethods.waitFor(3);
 //        Error: User with email fake@bluerentalcars.com not found
-        ReusableMethods.verifyElementDisplayed(loginPage.errorMessage_incorrectEmailPass);
-        String errorMessage=loginPage.errorMessage_incorrectEmailPass.getText();
+        String errorMessage = loginPage.errorMessage_incorrectEmailPass.getText();
         Assert.assertEquals(errorMessage,"User with email fake@bluerentalcars.com not found");
         ReusableMethods.getScreenshot("NegativeLoginScreenshot");
-        Driver.closeDriver();
-
     }
     @Test
     public void invalidCredsTest() throws IOException {
-
-        //  As customer, I should not be able to log in the application
+//        As customer, I should not be able to log in the application
+        //going blue rental car home page
         Driver.getDriver().get(ConfigReader.getProperty("app_home_url"));
-        //        click on home page login link
+//        click on home page login link
         homePage = new HomePage();
-        loginPage=new LoginPage();
+        loginPage= new LoginPage();
         ReusableMethods.waitFor(3);
-        //  Customer email: fake@bluerentalcars.com
-        faker=new Faker();
-        String fakeEmail= faker.internet().emailAddress();
         homePage.homePageLoginLink.click();
+//        sending credentials and clicking on login button
         ReusableMethods.waitFor(3);
+//        Customer email: fake@bluerentalcars.com
+//        Customer password: fakepass
+        faker=new Faker();
+        String fakeEmail = faker.internet().emailAddress();
         loginPage.userName.sendKeys(fakeEmail);
         ReusableMethods.waitFor(3);
-        //  Customer password: fakepass
-        loginPage.password.sendKeys(faker.internet().password(4,6));//fake password 4-6 digit
+        loginPage.password.sendKeys(faker.internet().password(4,6));//fake pass 4-6 digit
         ReusableMethods.waitFor(3);
         loginPage.loginButton.click();
         ReusableMethods.waitFor(3);
 //        Error: User with email fake@bluerentalcars.com not found
-        ReusableMethods.verifyElementDisplayed(loginPage.errorMessage_incorrectEmailPass);
-        String errorMessage=loginPage.errorMessage_incorrectEmailPass.getText();
+        String errorMessage = loginPage.errorMessage_incorrectEmailPass.getText();
         Assert.assertEquals(errorMessage,"User with email "+fakeEmail+" not found");
         ReusableMethods.getScreenshot("NegativeLoginScreenshot");
         Driver.closeDriver();
-
     }
 }
